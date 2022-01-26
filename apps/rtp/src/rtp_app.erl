cd9b47b933f71df2fd7138ea1b551d63644c0265
Volcov Oleg <bojikch15@gmail.com>
2022-01-26 21:08:10 +0200

    Refactor all `rtp` erlang files from default.
     + Add `io` to test is application running or not.

--- apps/rtp/src/rtp_app.erl
+++ apps/rtp/src/rtp_app.erl
@@ -1,18 +1,31 @@
 %%%-------------------------------------------------------------------
-%% @doc rtp public API
-%% @end
+%%% @author Volcov Oleg
+%%% @copyright (C) 2022, FAF-191
+%%% @doc Tweet-Analyzer application entry point.
+%%% @end
 %%%-------------------------------------------------------------------
 
 -module(rtp_app).
+-author("Volcov Oleg").
 
 -behaviour(application).
 
+%% OTP application entry point.
 -export([start/2, stop/1]).
+%% Tweet-Analyzer application entry point.
+-export([start/0, stop/0]).
+
+start() ->
+    application:start(?MODULE).
 
 start(_StartType, _StartArgs) ->
+    io:format("[~p] Tweet-Analyzer's `start` is called.~n", [self()]),
     rtp_sup:start_link().
 
+stop() ->
+    application:stop(?MODULE).
+
 stop(_State) ->
+    io:format("[~p] Tweet-Analyzer's `stop` is called.~n", [self()]),
     ok.
 
-%% internal functions
--- apps/rtp/src/rtp_sup.erl
+++ apps/rtp/src/rtp_sup.erl
@@ -1,35 +1,31 @@
 %%%-------------------------------------------------------------------
-%% @doc rtp top level supervisor.
-%% @end
+%%% @author Volcov Oleg
+%%% @copyright (C) 2022, FAF-191
+%%% @doc Tweet-Analyzer main supervisor.
+%%% @end
 %%%-------------------------------------------------------------------
 
 -module(rtp_sup).
+-author("Volcov Oleg").
 
 -behaviour(supervisor).
 
 -export([start_link/0]).
-
 -export([init/1]).
 
--define(SERVER, ?MODULE).
-
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
+    io:format("[~p] Tweet-Analyzer' superviser's `init` is called.~n", [self()]),
+
+    MaxRestarts = 100,
+    MaxSecondsBetweenRestarts = 10,
+    SupFlags = #{
+        strategy => one_for_one,
+        intensity => MaxRestarts,
+        period => MaxSecondsBetweenRestarts
+    },
+
     ChildSpecs = [],
     {ok, {SupFlags, ChildSpecs}}.
-
-%% internal functions
