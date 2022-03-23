8b1341c4bb250d2098a41bd29fbf06770afa3fcc
Volcov Oleg <bojikch15@gmail.com>
2022-03-23 15:51:24 +0200

    Update scripts to send data to `aggregator`.

--- apps/rtp/src/worker_group/worker_type/engagement_ratio_processing.erl
+++ apps/rtp/src/worker_group/worker_type/engagement_ratio_processing.erl
@@ -28,9 +28,11 @@ work(Tweet) ->
 	#{<<"favorite_count">> := FavoriteCount} = Tweet,
 	#{<<"retweet_count">> := RetweetCount} = Tweet,
 
+	#{<<"id">> := ID} = Tweet,
+
 	EngageRation = engagement_ration_calculation(FavoriteCount, RetweetCount, FollowersCount),
 
-	io:format("[~p] worker is processed data with `Engagement Ratio`=`~p`.~n", [self(), EngageRation]).
+	gen_server:cast(aggregator, {engage_ratio, ID, EngageRation}).
 
 engagement_ration_calculation(_, _, FollowersCount) when FollowersCount  =:= 0 ->
 	0;
