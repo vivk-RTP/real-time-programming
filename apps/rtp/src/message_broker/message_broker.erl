bd07ef44591cb361a7eb371aae0131eebcc689cb
Volcov Oleg <bojikch15@gmail.com>
2022-03-29 16:30:41 +0300

    Implement `message_broker` publish, subscribe, unsubscribe functional.

--- apps/rtp/src/message_broker/message_broker.erl
+++ apps/rtp/src/message_broker/message_broker.erl
@@ -29,6 +29,17 @@ init([]) ->
 handle_call(_Request, _From, State = #message_broker_state{}) ->
 	{reply, ok, State}.
 
+handle_cast({subscribe, Attribute, DestinationPID}, State = #message_broker_state{subscribers = OldSubscribers}) ->
+	Subscribers = subscribe(Attribute, DestinationPID, OldSubscribers),
+	NewState = State#message_broker_state{subscribers = Subscribers},
+	{noreply, NewState};
+handle_cast({unsubscribe, Attribute, DestinationPID}, State = #message_broker_state{subscribers = OldSubscribers}) ->
+	Subscribers = unsubscribe(Attribute, DestinationPID, OldSubscribers),
+	NewState = State#message_broker_state{subscribers = Subscribers},
+	{noreply, NewState};
+handle_cast({publish, Attribute, Message}, State = #message_broker_state{subscribers = Subscribers}) ->
+	publish(Attribute, Message, Subscribers),
+	{noreply, State};
 handle_cast(_Request, State = #message_broker_state{}) ->
 	{noreply, State}.
 
@@ -52,3 +63,39 @@ get_specs() ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+subscribe(Attribute, Subscriber, SubsMap) ->
+	Found = maps:find(Attribute, SubsMap),
+	add_subscriber(Found, Attribute, Subscriber, SubsMap).
+
+add_subscriber({ok, OldSubs}, Attribute, Subscriber, SubsMap) ->
+	SubsMap#{Attribute := [Subscriber|OldSubs]};
+add_subscriber(error, Attribute, Subscriber, SubsMap) ->
+	SubsMap#{Attribute => [Subscriber]}.
+
+unsubscribe(Attribute, Subscriber, SubsMap) ->
+	Found = maps:find(Attribute, SubsMap),
+	remove_subscriber(Found, Attribute, Subscriber, SubsMap).
+
+remove_subscriber({ok, OldSubs}, Attribute, Subscriber, SubsMap) ->
+	Subs = lists:delete(Subscriber, OldSubs),
+	SubsMap#{Attribute := Subs};
+remove_subscriber(error, _, _, SubsMap) ->
+	SubsMap.
+
+publish(Attribute, Message, SubsMap) ->
+	Found = maps:find(Attribute, SubsMap),
+	send_message(Found, Attribute, Message).
+
+send_message({ok, Subs}, Attribute, Message) ->
+	send_message_loop(Subs, Attribute, Message);
+send_message(error, _, _) ->
+	ok.
+
+send_message_loop([], _, _) ->
+	ok;
+send_message_loop([Head|Tail], Attribute, Message) ->
+	gen_server:cast(Head, {Attribute, Message}),
+	send_message_loop(Tail, Attribute, Message).
+
+
