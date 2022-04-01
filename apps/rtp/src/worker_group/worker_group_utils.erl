48bc4632dd00ee4748c0428fc013d49608d1a99f
Volcov Oleg <bojikch15@gmail.com>
2022-04-01 10:49:01 +0300

    Add new functions to get `worker_manager`, `worker_scalre` and `worker_sup` by supervisor pid.

--- apps/rtp/src/worker_group/worker_group_utils.erl
+++ apps/rtp/src/worker_group/worker_group_utils.erl
@@ -6,7 +6,7 @@
 %%%-------------------------------------------------------------------
 -module(worker_group_utils).
 
--export([get_specs/2]).
+-export([get_specs/2, get_manager/1, get_scaler/1, get_worker_sup/1]).
 
 %%%===================================================================
 %%% External functions
@@ -16,3 +16,36 @@ get_specs(ID, WorkerHandler) ->
 	Worker = worker:get_specs(ID, WorkerHandler),
 	WorkerGroupSup = worker_group_sup:get_specs(ID, Worker),
 	WorkerGroupSup.
+
+get_manager(SelfPID) ->
+	AllChildren = supervisor:which_children(SelfPID),
+	[Child|_] = get_child_by_modules(AllChildren, [], worker_manager),
+	Child.
+
+get_scaler(SelfPID) ->
+	AllChildren = supervisor:which_children(SelfPID),
+	[Child|_] = get_child_by_modules(AllChildren, [], worker_scaler),
+	Child.
+
+get_worker_sup(SelfPID) ->
+	AllChildren = supervisor:which_children(SelfPID),
+	[Child|_] = get_child_by_modules(AllChildren, [], worker_sup),
+	Child.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+get_child_by_modules([], Acc, _) ->
+	Acc;
+get_child_by_modules([Head|Tail], Acc, Module) ->
+	{_, PID, _, Modules} = Head,
+	IsMember = lists:member(Module, Modules),
+	NewAcc = is_member(IsMember, PID, Acc),
+	get_child_by_modules(Tail, NewAcc, Module).
+
+
+is_member(true, PID, Acc) ->
+	[PID|Acc];
+is_member(false, _, Acc) ->
+	Acc.
