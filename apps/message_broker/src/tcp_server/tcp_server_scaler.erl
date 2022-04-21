eaa7f76fd6a588c643de37d763a478b650cb6192
Volcov Oleg <bojikch15@gmail.com>
2022-04-21 10:06:48 +0300

    Create tcp server worker group file layout.

---
+++ apps/message_broker/src/tcp_server/tcp_server_scaler.erl
@@ -0,0 +1,46 @@
+%%%-------------------------------------------------------------------
+%%% @author Oleg
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%% @end
+%%%-------------------------------------------------------------------
+-module(tcp_server_scaler).
+
+-behaviour(gen_server).
+
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+	code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(tcp_server_scaler_state, {}).
+
+%%%===================================================================
+%%% Spawning and gen_server implementation
+%%%===================================================================
+
+start_link() ->
+	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+	{ok, #tcp_server_scaler_state{}}.
+
+handle_call(_Request, _From, State = #tcp_server_scaler_state{}) ->
+	{reply, ok, State}.
+
+handle_cast(_Request, State = #tcp_server_scaler_state{}) ->
+	{noreply, State}.
+
+handle_info(_Info, State = #tcp_server_scaler_state{}) ->
+	{noreply, State}.
+
+terminate(_Reason, _State = #tcp_server_scaler_state{}) ->
+	ok.
+
+code_change(_OldVsn, State = #tcp_server_scaler_state{}, _Extra) ->
+	{ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
---
+++ apps/message_broker/src/tcp_server/tcp_server_sup.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% @author Oleg
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%% @end
+%%%-------------------------------------------------------------------
+-module(tcp_server_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+	AChild = #{id => 'AName',
+		start => {'AModule', start_link, []},
+		restart => permanent,
+		shutdown => 2000,
+		type => worker,
+		modules => ['AModule']},
+
+	{ok, {#{strategy => one_for_one,
+		intensity => 5,
+		period => 30},
+		[AChild]}
+	}.
