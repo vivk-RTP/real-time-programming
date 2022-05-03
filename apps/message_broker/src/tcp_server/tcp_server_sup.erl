54fb461678a017010422b6fb5f92daac43a20ae9
Volcov Oleg <bojikch15@gmail.com>
2022-05-03 17:19:16 +0300

    Modify `tcp_server_sup` to run full `tcp working group`.

--- apps/message_broker/src/tcp_server/tcp_server_sup.erl
+++ apps/message_broker/src/tcp_server/tcp_server_sup.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
-%%% @author Oleg
-%%% @copyright (C) 2022, <COMPANY>
+%%% @author Volcov Oleg
+%%% @copyright (C) 2022, FAF-191
 %%% @doc
 %%% @end
 %%%-------------------------------------------------------------------
@@ -9,20 +9,43 @@
 -behaviour(supervisor).
 
 -export([start_link/0, init/1]).
+-export([get_specs/0]).
 
 start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	AChild = #{id => 'AName',
-		start => {'AModule', start_link, []},
-		restart => permanent,
-		shutdown => 2000,
-		type => worker,
-		modules => ['AModule']},
+	io:format("[~p] tcp_server_sup superviser's `init` is called.~n", [self()]),
 
-	{ok, {#{strategy => one_for_one,
-		intensity => 5,
-		period => 30},
-		[AChild]}
+	MaxRestarts = 5000,
+	MaxSecondsBetweenRestarts = 10,
+	SupFlags = #{
+		strategy => one_for_one,
+		intensity => MaxRestarts,
+		period => MaxSecondsBetweenRestarts
+	},
+
+	TCPSocketPoolSup = tcp_socket_pool_sup:get_specs(),
+	TCPServerScaler = tcp_server_scaler:get_specs(),
+
+	ChildSpecs = [
+		TCPSocketPoolSup,
+		TCPServerScaler
+	],
+
+	{ok, {SupFlags, ChildSpecs}}.
+
+%%%===================================================================
+%%% External functions
+%%%===================================================================
+
+get_specs() ->
+	#{
+		id => tcp_server_sup,
+		start => {tcp_server_sup, start_link, []},
+		restart => permanent,
+		shutdown => infinity,
+		type => supervisor,
+		modules => [tcp_server_sup]
 	}.
+
