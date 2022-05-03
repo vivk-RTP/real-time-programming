d3a851d574cfb152471b57aa71d563aef44389aa
Volcov Oleg <bojikch15@gmail.com>
2022-05-03 17:14:45 +0300

    Modify `tcp_socket_pool_sup` to create `listener` socket and create new `tcp_socket` workers.

--- apps/message_broker/src/tcp_server/tcp_socket_pool_sup.erl
+++ apps/message_broker/src/tcp_server/tcp_socket_pool_sup.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
-%%% @author Oleg
-%%% @copyright (C) 2022, <COMPANY>
+%%% @author Volcov Oleg
+%%% @copyright (C) 2022, FAF-191
 %%% @doc
 %%% @end
 %%%-------------------------------------------------------------------
@@ -9,20 +9,53 @@
 -behaviour(supervisor).
 
 -export([start_link/0, init/1]).
+-export([get_specs/0, start_worker/0]).
+
+-define(PORT, 25250).
+-define(OPTIONS, [{active, true}, {packet, 0}]).
 
 start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	AChild = #{id => 'AName',
-		start => {'AModule', start_link, []},
-		restart => permanent,
-		shutdown => 2000,
-		type => worker,
-		modules => ['AModule']},
+	MaxRestarts = 5000,
+	MaxSecondsBetweenRestarts = 10,
+	SupFlags = #{
+		strategy => simple_one_for_one,
+		intensity => MaxRestarts,
+		period => MaxSecondsBetweenRestarts
+	},
 
-	{ok, {#{strategy => one_for_one,
-		intensity => 5,
-		period => 30},
-		[AChild]}
+	io:format("[~p] tcp_socket_pool_sup is started!~n", [self()]),
+	{ok, NewSocket} = gen_tcp:listen(?PORT, ?OPTIONS),
+	io:format("[~p] tcp_socket_pool_sup server started with Socket=[~p]~n", [self(), NewSocket]),
+
+	TCPSocket = tcp_socket:get_specs(NewSocket),
+
+	Children = [
+		TCPSocket
+	],
+
+	{ok, {SupFlags, Children}}.
+
+%%%===================================================================
+%%% External functions
+%%%===================================================================
+
+get_specs() ->
+	#{
+		id => tcp_socket_pool_sup,
+		start => {tcp_socket_pool_sup, start_link, []},
+		restart => permanent,
+		shutdown => infinity,
+		type => supervisor,
+		modules => [tcp_socket_pool_sup]
 	}.
+
+start_worker() ->
+	{ok, WorkerPID} = supervisor:start_child(?MODULE, []),
+	WorkerPID.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
